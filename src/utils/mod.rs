d14af929f61000bc15bfd6dda1c45c01a814bf07
gquesnot <gaqu1994@gmail.com>
2024-07-10 20:16:04 +0200

    fix save_json_to_file

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -3,7 +3,11 @@ use serde_json::Value;
 pub mod process_info;
 
 pub fn save_json_to_file(name: &str, json: &Value) -> std::io::Result<()> {
-    let file = std::fs::File::create(std::path::Path::new("temp").join(name))?;
+    let temp = std::path::Path::new("temp");
+    if !temp.exists() {
+        std::fs::create_dir(temp)?;
+    }
+    let file = std::fs::File::create(temp.join(name))?;
     serde_json::to_writer_pretty(file, json)?;
     Ok(())
 }
